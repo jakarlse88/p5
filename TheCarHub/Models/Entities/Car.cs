7740e3706ea506bdc51b213698cfe0828bc19c7f
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-11-04 10:17:10 +0100

    Revert "Car entity: year property to string"
    This reverts commit 46cecfab370e639ef1875f59d18061e867b5bcce.

--- TheCarHub/Models/Entities/Car.cs
+++ TheCarHub/Models/Entities/Car.cs
@@ -8,7 +8,7 @@ namespace TheCarHub.Models
         public int Id { get; set; }
         public List<Listing> Listings { get; set; }
         public string VIN { get; set; }
-        public string Year { get; set; }
+        public DateTime Year { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public string Trim { get; set; }
