af68147dc5701497262f019fe432535a07cab1ef
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-11-17 20:02:05 +0100

    Bug: reintroduce Description property on RepairJob entity

--- TheCarHub/Models/Entities/RepairJob.cs
+++ TheCarHub/Models/Entities/RepairJob.cs
@@ -5,6 +5,7 @@ namespace TheCarHub.Models.Entities
         public int Id { get; set; }
         public int ListingId { get; set; }
         public Listing Listing { get; set; }
+        public string Description { get; set; }
         public decimal Cost { get; set; }
         public decimal Tax { get; set; }
     }
