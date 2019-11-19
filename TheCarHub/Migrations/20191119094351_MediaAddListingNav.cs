d62ca48722ff36f898fb7ed2bf32d83e07c499b4
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-11-19 10:44:24 +0100

    Add Listing nav property to Media entity

---
+++ TheCarHub/Migrations/20191119094351_MediaAddListingNav.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheCarHub.Migrations
+{
+    public partial class MediaAddListingNav : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
